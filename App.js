ebd91daf73b6a30a41685b6f5a6364bd72aa55bd
chitraw <85836941+chitraw@users.noreply.github.com>
2022-12-14 11:40:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,138 @@
+
+function App() {
+  return (
+    <form>
+        <label>
+          Pick your favorite flavor:
+          <select >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+        <br></br>
+
+        <label> 
+          Journel type:
+          <select>
+            <option value = "1"> 1 </option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" /> 
+        <t>                                                     
+
+        </t>
+
+        <label> 
+         Dec NO:
+         <select>
+         <option value = "1"> 1 </option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         </select>
+         </label>
+         <input type="submit" value="Submit" />
+
+
+         <label> 
+         System Voucher:
+         <select>
+         <option value = "1"> 1 </option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         </select>
+         </label>
+         <input type="submit" value="Submit" />
+
+
+
+         <label> 
+         Currency:
+         <select>
+         <option value = "1"> 1 </option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         </select>
+         </label>
+         <input type="submit" value="Submit" />
+
+
+         <label> 
+         Duvision:
+         <select>
+         <option value = "1"> 1 </option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         </select>
+         </label>
+         <input type="submit" value="Submit" />
+
+
+         <label> 
+         Payment mode:
+         <select>
+         <option value = "1"> 1 </option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         </select>
+         </label>
+         <input type="submit" value="Submit" />
+
+         <label> 
+         Petty Cash A ccount:
+         <select>
+         <option value = "1"> 1 </option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         </select>
+         </label>
+         <input type="submit" value="Submit" />
+
+
+         <label> 
+         PO no :
+         <select>
+         <option value = "1"> 1 </option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         </select>
+         </label>
+         <input type="submit" value="Submit" />
+         </form>
+
+         
+
+         
+
+
+        
+                                           
+
+
+      
+
+      
+
+      
+      
+    
+  
+
+
+    
+  );
+}
+
+export default App;
